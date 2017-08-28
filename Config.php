2ae263f083dd365bc61aaa6652fc2859ad882a59
Aikay-Egwu <justaikay@gmail.com>
2017-08-28 08:57:13 +0100

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,32 @@
+<?php
+use Codenest\Library\Config as Config ;
+
+function configure_settings()
+{
+    Config::set('site_name', 'MVC');
+    Config::set('languages', array('en')) ;
+    //routes.  Route name => method prefix
+    Config::set('routes', array(
+        'default' => '',
+        'admin' => 'admin',
+        'jquery' => 'jquery'
+    ));
+    //default page
+    Config::set('default_route', 'Default') ;
+    //default site language
+    Config::set('default_language', 'en') ;
+    //default controller
+    Config::set('default_controller', 'home') ;
+    //default action
+    Config::set('default_action', 'index') ;
+
+    //database settings
+    Config::set('host', 'mysql:host=localhost;');
+    //database username
+    Config::set('username', 'mappsdbuser');
+    //database password
+    Config::set('password', 'nlcQv5to&t6TY7dn');
+    //database name
+    Config::set('db_name', 'dbname=mapps_new;');
+    Config::set('charser', 'charset=utf8mb4');
+}
